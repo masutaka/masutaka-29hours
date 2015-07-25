0ab5d90f79346da4560e3dceff725df931ebca70
Takashi Masuda <masutaka.net@gmail.com>
2015-07-25 17:21:08 +0900

    Add basic settings for deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'masutaka-29hours'
+set :repo_url, 'git@github.com:masutaka/masutaka-29hours.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/opt/masutaka-29hours'
 
 # Default value for :scm is :git
 # set :scm, :git
