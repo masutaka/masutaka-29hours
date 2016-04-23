0bf0d59b6e5fa97687f6015b1fda4c60c6bcebf5
Takashi Masuda <masutaka.net@gmail.com>
2016-04-23 15:44:17 +0900

    Unlock capistrano-3.4.1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
+lock '3.4.1'
 
 set :application, 'masutaka-29hours'
 set :repo_url, 'git@github.com:masutaka/masutaka-29hours.git'
